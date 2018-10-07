0b286efaf30292a663b32392cbec4ea1ebb36dcd
Tony Rogvall <tony@rogvall.se>
2018-10-07 15:12:36 +0200

    sort code and tests

--- src/bresenham.erl
+++ src/bresenham.erl
@@ -8,9 +8,7 @@
 -module(bresenham).
 
 -export([draw_line/4]).
-
-sort_y(P0={_,Y0,_},P1={_,Y1,_}) when Y0 < Y1 -> {P0,P1};
-sort_y(P0,P1) -> {P1,P0}.
+-export([test_sort_2/0, test_sort_3/0]).
 
 draw_line(Pixmap,{X0,Y0},{X1,Y1},Color) ->
     draw_line(Pixmap,{X0,Y0,1.0},{X1,Y1,1.0},Color);
@@ -45,6 +43,58 @@ drawx_(Pixmap,X,Xi,X1,Y,D,Dx,Dy,Color) ->
 	    drawx_(Pixmap,X+Xi,Xi,X1,Y,D+2*Dy,Dx,Dy,Color)
     end.
 
+draw_triangle(Pixmap, P0, P1, P2, Color) ->
+    {Q0,Q01,Q02} = sort_y(P0,P1,P2),
+    {Q1,Q2} = sort_x(Q01,Q02),
+    %% left edge is Q0-Q1 right edge is Q0-Q2
+    ok.
+
+
 sign(X) when X < 0 -> -1;
 sign(X) when X > 0 -> 1;
 sign(_) -> 0.
+
+sort_x(P0={X0,_,_},P1={X1,_,_}) when X0 < X1 -> {P0,P1};
+sort_x(P0,P1) -> {P1,P0}.
+
+sort_y(P0={_,Y0,_},P1={_,Y1,_}) when Y0 < Y1 -> {P0,P1};
+sort_y(P0,P1) -> {P1,P0}.
+
+sort_y(P0={_,Y0,_},P1={_,Y1,_},P2={_,Y2,_}) ->
+    if Y0 =< Y1 -> %% Y0 Y1 x | Y0 x Y1 | x Y0 Y1
+	    if Y1 =< Y2 ->  %% Y0 Y1 Y2
+		    {P0,P1,P2};
+	       Y2 =< Y0 ->
+		    {P2,P0,P1};
+	       true ->
+		    {P0,P2,P1}
+	    end;
+       %% Y1 Y0 x | Y1 x Y0 | x Y1 Y0
+       Y0 =< Y2 -> {P1,P0,P2};
+       Y1 =< Y2 -> {P1,P2,P0};
+       true -> {P2,P1,P0}
+    end.
+
+
+test_sort_2() ->
+    A = random_point_3d(),
+    B = random_point_3d(),
+    {A1,B1} = sort_y(A,B),
+    element(2,A1) =< element(2,B1).
+
+test_sort_3() ->
+    A = random_point_3d(),
+    B = random_point_3d(),
+    C = random_point_3d(),
+    {A1,B1,C1} = sort_y(A,B,C),
+    (element(2,A1) =< element(2,B1)) andalso
+	(element(2,B1) =< element(2,C1)).
+
+random_point_3d() ->
+    {rand:uniform(65536),rand:uniform(65536),rand:uniform(65536)}.
+
+
+
+
+    
+    
